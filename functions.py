bacff99bfa95011dcc661e9b9a1d27100e317330
cristinamulas <cristinamulaslopez@gmail.com>
2020-02-18 16:08:02 -0500

    final books

--- functions.py
+++ functions.py
@@ -3,6 +3,17 @@ import pandas as pd
 import urllib.parse
 import urllib.error
 from bs4 import BeautifulSoup
+import pyforest
+import spacy
+import re
+import string
+from spacy.lang.en.stop_words import STOP_WORDS
+from spacy.tokens import Token
+from spacy import displacy
+nlp = spacy.load("en_core_web_sm")
+import matplotlib.pyplot as plt
+import seaborn as sns
+from wordcloud import WordCloud, STOPWORDS
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
 from sklearn import metrics
@@ -48,8 +59,61 @@ def getting_tittles( soup_):
     total_reviews = review_tittle_2 + reviews_tittle_1
     return total_reviews
 
+def clean_data(df_):
+    """Make lower case ,remove puntuation, remove new lines and remove additional punctuation"""
+    for col in df_.columns:
+        df_[col]= df_[col].apply(lambda x: x.lower())
+        df_[col] = df_[col].apply(lambda x: re.sub('[%s]' % re.escape(string.punctuation), ' ', x))
+        df_[col]= df_[col].apply(lambda x:re.sub('[‘’“”…]', '', x))
+        df_[col]= df_[col].apply(lambda x: re.sub('\w*\d\w*', '', x))
+        df_[col] = df_[col].apply(lambda x: re.sub('/(^| ).( |$)/', '', x))
+    return df_.head()
 
+def words_frequency (string):
+    dict_ = {}
+    all_words = ' '.join([text for text in string])
+    all_words = all_words.split()
+    for i in all_words:
+        if i not in dict_:
+            dict_[i] = 1
+        else:
+            dict_[i] += 1
+    words_df = pd.DataFrame({'word': list(dict_.keys()) , 'count':list(dict_.values())})
+    df_sort = words_df.nlargest(columns = 'count' , n = 20)
+    print(df_sort)
+    plt.figure(figsize=(20,5))
+    ax = sns.barplot(data=df_sort, x= "word", y = "count")
+    ax.set(ylabel = 'Count')
+    return plt.show()
 
+def show_wordcloud(data, title = None): 
+    wordcloud = WordCloud( collocations=False,
+        background_color='white',
+        stopwords=set(STOPWORDS),
+        max_words=50,
+        max_font_size=50, 
+        scale=4,
+        random_state=1 # chosen at random by flipping a coin; it was heads
+    ).generate(str(data))
+
+    fig = plt.figure(1, figsize=(12, 12))
+    plt.axis('off')
+    if title: 
+        fig.suptitle(title, fontsize=20)
+        fig.subplots_adjust(top=2.3)
+
+    plt.imshow(wordcloud)
+    plt.show()
+    
+def plot_binary(df , column):
+    ''' plot for the binary target '''
+    plt.figure(figsize=(8,6))
+    sns.countplot(df[column], order=["positive", "negative"], palette='Set3')
+    plt.title('Positive and Negative Reviews',fontsize=20)
+    plt.xlabel('Type of Review', fontsize=16)
+    plt.ylabel('Number of Reviews', fontsize=16)
+
+    
 def model (classifier, name, col1, col2):
     classifier_1 = classifier
     tfidf = TfidfVectorizer(sublinear_tf=True, min_df=5, norm='l2', encoding='latin-1', ngram_range = (1,2), stop_words='english')
